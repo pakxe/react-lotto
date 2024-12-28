ecc11e2842752cdab5cbd5c2f1257c8edb91128b
pakxe <pigkill40@naver.com>
2024-12-28 17:51:45 +0900

    feat: 모달상태 관리를 위임하여 해당 컴포넌트에서는 책임 회피

--- src/components/LottoShop/LottoShop.tsx
+++ src/components/LottoShop/LottoShop.tsx
@@ -1,69 +1,58 @@
 import { useTheme } from '@emotion/react';
 import Text from '../Text/Text';
 import { lottoShopContainerStyle } from './LottoShop.style';
-import Spacing from '../Spacing/Spacing';
 import Button from '../Button/Button';
 import LottoList from '../LottoList/LottoList';
 import LottoAnswerInput from '../LottoAnswerInput/LottoAnswerInput';
 import LottoPurchaseInput from '../LottoPurchaseInput/LottoPurchaseInput';
 import { useState } from 'react';
-import LottoResultBottomSheet from '../LottoResultBottomSheet/LottoResultBottomSheet';
-import { LottoNumber } from '../../serviceType';
-import { useLottoContext } from '../../hooks/useLotto';
-
-const dummyLottoList = [
-  {
-    id: 1,
-    numbers: [1, 2, 3, 4, 5, 6],
-  },
-  {
-    id: 2,
-    numbers: [10, 20, 30, 40, 50, 60],
-  },
-  {
-    id: 3,
-    numbers: [11, 22, 33, 44, 55, 66],
-  },
-];
+import useLottoResultBottomSheet from '../../hooks/useLottoResultBottomSheet';
+import { useLottoContext } from '../../contexts/LottoContext';
 
 const LottoShop = () => {
-  const [isBottomSheetOpen, setIsBottomSheetOpen] = useState(false);
-  // TODO: wow..
-  const [lottoNumbers, setLottoNumbers] = useState<{ numbers: LottoNumber[]; bonusNumber: LottoNumber }>({
-    numbers: [],
-    bonusNumber: 0,
+  const { open } = useLottoResultBottomSheet();
+  const theme = useTheme();
+  const { updateLottoAnswer, lottos, buyLotto, amount, lottoAnswer, lottoAnswerErrorMessage, amountErrorMessage } =
+    useLottoContext();
+
+  const [lottoAnswerInput, setLottoAnswerInput] = useState<{ numbers: string[]; bonusNumber: string }>({
+    numbers: new Array(6).fill(''),
+    bonusNumber: '',
   });
 
-  const theme = useTheme();
+  const checkResult = () => {
+    if (lottoAnswerErrorMessage !== '') {
+      alert(lottoAnswerErrorMessage);
+      return;
+    }
 
-  const closeBottomSheet = () => {
-    setIsBottomSheetOpen(false);
-  };
-
-  const openBottomSheet = () => {
-    setIsBottomSheetOpen(true);
+    open();
+    updateLottoAnswer({ numbers: lottoAnswer.numbers, bonusNumber: lottoAnswer.bonusNumber });
   };
 
   return (
-    <>
-      <section css={lottoShopContainerStyle({ theme })}>
-        <Text type='title' textAlign='center'>
-          내 번호 당첨 확인
-        </Text>
-        <Spacing height={20} />
+    <section css={lottoShopContainerStyle({ theme })}>
+      <Text type='title' textAlign='center'>
+        내 번호 당첨 확인
+      </Text>
 
-        <LottoPurchaseInput />
+      <LottoPurchaseInput buyLotto={buyLotto} amount={amount} errorMessage={amountErrorMessage} />
 
-        <LottoList lottoList={dummyLottoList} />
+      {lottos.length !== 0 && (
+        <>
+          <LottoList />
 
-        <LottoAnswerInput setLottoNumbers={setLottoNumbers} lottoNumbers={lottoNumbers} />
-        <Button onClick={openBottomSheet} fullWidth>
-          결과 확인하기
-        </Button>
-      </section>
-
-      {isBottomSheetOpen && <LottoResultBottomSheet closeBottomSheet={closeBottomSheet} />}
-    </>
+          <LottoAnswerInput
+            setLottoAnswer={setLottoAnswerInput}
+            lottoAnswer={lottoAnswerInput}
+            lottoAnswerDefault={lottoAnswer}
+          />
+          <Button onClick={checkResult} fullWidth>
+            결과 확인하기
+          </Button>
+        </>
+      )}
+    </section>
   );
 };
 

73d19342c97548d77f252c905891f5d47e7a2d2f
pakxe <pigkill40@naver.com>
2024-12-28 19:38:09 +0900

    feat: 유효성 검사 책임을 다른 훅으로 위임

--- src/hooks/useLottoShop.ts
+++ src/hooks/useLottoShop.ts
@@ -6,36 +6,8 @@ import { Lotto } from '../serviceType';
 const useLottoShop = () => {
   const [amount, setAmount] = useState(0);
   const [lottos, setLottos] = useState<Lotto[]>([]);
-  const [errorMessage, setErrorMessage] = useState('');
-
-  const handleError = (message: string): boolean => {
-    setErrorMessage(message);
-    return false;
-  };
-
-  const checkAmount = (amount: number): boolean => {
-    if (Number.isNaN(amount)) {
-      return handleError('숫자만 입력해주세요.');
-    }
-
-    if (amount < LOTTO_PRICE) {
-      return handleError('로또 한 장의 가격은 1000원입니다.');
-    }
-
-    if (amount % LOTTO_PRICE !== 0) {
-      return handleError('1000원 단위로 입력해주세요.');
-    }
-
-    setAmount(amount);
-    setErrorMessage('');
-    return true;
-  };
 
   const buyLotto = (amount: number) => {
-    if (!checkAmount(amount)) {
-      return;
-    }
-
     const lottos = generateLottos(Math.trunc(amount / LOTTO_PRICE));
 
     setLottos(
@@ -57,7 +29,6 @@ const useLottoShop = () => {
     buyLotto,
     lottos,
     amount,
-    errorMessage,
     clearLottoShop,
   };
 };

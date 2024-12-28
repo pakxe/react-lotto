35fc190c37a7c53a886f547e1e91a28f5d94741a
pakxe <pigkill40@naver.com>
2024-12-28 17:52:05 +0900

    feat: matchCount 출력

--- src/components/LottoResultTable/LottoResultTable.tsx
+++ src/components/LottoResultTable/LottoResultTable.tsx
@@ -1,6 +1,8 @@
 import { useTheme } from '@emotion/react';
 import { lottoResultTableStyle } from './LottoResultTable.style';
 import { LOTTO_REWARDS } from '../../constants/lotto';
+import { useLottoContext } from '../../contexts/LottoContext';
+import calcMatchCountResult from '../../utils/calcMatchCountResult';
 
 const REWARD_TABLE = LOTTO_REWARDS.map((reward, index) => {
   let name;
@@ -22,21 +24,24 @@ const REWARD_TABLE = LOTTO_REWARDS.map((reward, index) => {
 const LottoResultTable = () => {
   const theme = useTheme();
 
+  const { lottoAnswer, lottos } = useLottoContext();
+  const matchCountResult = calcMatchCountResult({ ...lottoAnswer, lottos });
+
   return (
     <table css={lottoResultTableStyle({ theme })}>
       <thead>
         <tr>
-          <th scope="col">일치 개수</th>
-          <th scope="col">당첨금</th>
-          <th scope="col">당첨 개수</th>
+          <th scope='col'>일치 개수</th>
+          <th scope='col'>당첨금</th>
+          <th scope='col'>당첨 개수</th>
         </tr>
       </thead>
       <tbody>
-        {REWARD_TABLE.map(({ name, reward }) => (
+        {REWARD_TABLE.map(({ name, reward }, index) => (
           <tr key={name}>
             <td>{name}</td>
             <td>{reward.toLocaleString('ko-kr')}</td>
-            <td>30개</td>
+            <td>{matchCountResult[index]}개</td>
           </tr>
         ))}
       </tbody>

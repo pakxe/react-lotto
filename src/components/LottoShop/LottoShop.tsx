07387f795f47189279a9b2622c7e70301e101131
pakxe <pigkill40@naver.com>
2024-12-28 19:35:06 +0900

    feat: 불필요한 상태 선언제거

--- src/components/LottoShop/LottoShop.tsx
+++ src/components/LottoShop/LottoShop.tsx
@@ -3,29 +3,19 @@ import Text from '../Text/Text';
 import { lottoShopContainerStyle } from './LottoShop.style';
 import Button from '../Button/Button';
 import LottoList from '../LottoList/LottoList';
-import LottoAnswerInput from '../LottoAnswerInput/LottoAnswerInput';
-import LottoPurchaseInput from '../LottoPurchaseInput/LottoPurchaseInput';
+import LottoAnswerInputSection from '../LottoAnswerInput/LottoAnswerInput';
+import LottoPurchaseInputSection from '../LottoPurchaseInput/LottoPurchaseInput';
 import { useState } from 'react';
 import useLottoResultBottomSheet from '../../hooks/useLottoResultBottomSheet';
 import { useLottoContext } from '../../contexts/LottoContext';
+import { LottoAnswer } from '../../serviceType';
 
 const LottoShop = () => {
   const { open } = useLottoResultBottomSheet();
   const theme = useTheme();
-  const { updateLottoAnswer, lottos, buyLotto, amount, lottoAnswer, lottoAnswerErrorMessage, amountErrorMessage } =
-    useLottoContext();
+  const { updateLottoAnswer, lottos, buyLotto, lottoAnswer, lottoAnswerErrorMessage, amount } = useLottoContext();
 
-  const [lottoAnswerInput, setLottoAnswerInput] = useState<{ numbers: string[]; bonusNumber: string }>({
-    numbers: new Array(6).fill(''),
-    bonusNumber: '',
-  });
-
-  const checkResult = () => {
-    if (lottoAnswerErrorMessage !== '') {
-      alert(lottoAnswerErrorMessage);
-      return;
-    }
-
+  const checkResult = (lottoAnswer: LottoAnswer) => {
     open();
     updateLottoAnswer({ numbers: lottoAnswer.numbers, bonusNumber: lottoAnswer.bonusNumber });
   };
@@ -36,20 +26,13 @@ const LottoShop = () => {
         내 번호 당첨 확인
       </Text>
 
-      <LottoPurchaseInput buyLotto={buyLotto} amount={amount} errorMessage={amountErrorMessage} />
+      <LottoPurchaseInputSection buyLotto={buyLotto} defaultAmount={amount} />
 
       {lottos.length !== 0 && (
         <>
           <LottoList />
 
-          <LottoAnswerInput
-            setLottoAnswer={setLottoAnswerInput}
-            lottoAnswer={lottoAnswerInput}
-            lottoAnswerDefault={lottoAnswer}
-          />
-          <Button onClick={checkResult} fullWidth>
-            결과 확인하기
-          </Button>
+          <LottoAnswerInputSection onSubmit={checkResult} lottoAnswerDefault={lottoAnswer} />
         </>
       )}
     </section>

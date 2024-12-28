8d9a020f25beaf3a4209f8a33da522b97a752a0f
pakxe <pigkill40@naver.com>
2024-12-28 17:52:26 +0900

    fix: restart 로직 붙임

--- src/components/LottoResultBottomSheet/LottoResultBottomSheet.tsx
+++ src/components/LottoResultBottomSheet/LottoResultBottomSheet.tsx
@@ -1,20 +1,29 @@
-import { useTheme } from '@emotion/react';
 import BottomSheet from '../BottomSheet/BottomSheet';
 import Button from '../Button/Button';
 import Text from '../Text/Text';
-import { lottoResultBottomSheetStyle } from './LottoResultBottomSheet.style';
 import LottoResultTable from '../LottoResultTable/LottoResultTable';
-import { useLottoContext } from '../../hooks/useLotto';
+import { useLottoContext } from '../../contexts/LottoContext';
+import calcMatchCountResult from '../../utils/calcMatchCountResult';
+import calcRateOfReturn from '../../utils/calcRateOfReturn';
+import calcTotalReward from '../../utils/calcTotalReward';
 
 type Props = {
-  closeBottomSheet: () => void; // TODO: 드릴링
+  isOpen: boolean;
+  close: () => void;
 };
 
-const LottoResultBottomSheet = ({ closeBottomSheet }: Props) => {
-  const { getMatchCountResult, getTotalReward } = useLottoContext();
+const LottoResultBottomSheet = ({ isOpen, close }: Props) => {
+  const { clear, lottoAnswer, lottos, amount } = useLottoContext();
+  const matchCount = calcMatchCountResult({ ...lottoAnswer, lottos });
+  const totalReward = calcTotalReward(matchCount);
+
+  const restart = () => {
+    clear();
+    close();
+  };
 
   return (
-    <BottomSheet cssStyle={{ gap: '32px' }} closeBottomSheet={closeBottomSheet}>
+    <BottomSheet cssStyle={{ gap: '32px' }} close={close} isOpen={isOpen}>
       <Text type='subtitle' textAlign='center'>
         당첨 통계
       </Text>
@@ -22,9 +31,11 @@ const LottoResultBottomSheet = ({ closeBottomSheet }: Props) => {
       <LottoResultTable />
 
       <Text type='bodyBold' textAlign='center'>
-        당신의 총 수익률은 %입니다.
+        당신의 총 수익률은 {calcRateOfReturn(totalReward, amount)}%입니다.
       </Text>
-      <Button fullWidth>다시 시작하기</Button>
+      <Button onClick={restart} fullWidth>
+        다시 시작하기
+      </Button>
     </BottomSheet>
   );
 };

b7816a3693ccb9f3fabeca2cb001cfbd952e333d
pakxe <pigkill40@naver.com>
2024-12-28 17:47:28 +0900

    refactor: props 제거

--- src/components/LottoList/LottoList.tsx
+++ src/components/LottoList/LottoList.tsx
@@ -1,24 +1,22 @@
-import { Lotto } from '../../serviceType';
+import { useLottoContext } from '../../contexts/LottoContext';
 import LottoItem from '../LottoItem/LottoItem';
 import Spacing from '../Spacing/Spacing';
 import Text from '../Text/Text';
 import { lottoListStyle } from './LottoList.style';
 
-type Props = {
-  lottoList: Lotto[];
-};
+const LottoList = () => {
+  const { lottos } = useLottoContext();
 
-const LottoList = ({ lottoList }: Props) => {
   return (
-    <>
-      <Text>총 {lottoList.length}개를 구매하였습니다.</Text>
+    <div>
+      <Text>총 {lottos.length}개를 구매하였습니다.</Text>
       <Spacing height={8} />
       <ol css={lottoListStyle}>
-        {lottoList.map((lotto) => (
+        {lottos.map((lotto) => (
           <LottoItem lotto={lotto} />
         ))}
       </ol>
-    </>
+    </div>
   );
 };
 

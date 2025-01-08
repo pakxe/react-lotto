5a6602a796666988067ed399a01e47c3942e9b6c
pakxe <pigkill40@naver.com>
2025-01-08 12:43:28 +0900

    refactor: 매직넘버를 사용하도록 수정

--- src/utils/generateLottos.ts
+++ src/utils/generateLottos.ts
@@ -1,11 +1,13 @@
+import { LOTTO_NUMBER_COUNT, LOTTO_RANGE } from '../constants/lotto';
+
 const generateLottos = (count: number) => {
   const lottos = [];
 
   for (let i = 0; i < count; i++) {
     const lotto: number[] = [];
 
-    while (lotto.length < 6) {
-      const number = Math.floor(Math.random() * 45) + 1;
+    while (lotto.length < LOTTO_NUMBER_COUNT) {
+      const number = Math.floor(Math.random() * LOTTO_RANGE.MAX) + 1;
       if (!lotto.includes(number)) {
         lotto.push(number);
       }
